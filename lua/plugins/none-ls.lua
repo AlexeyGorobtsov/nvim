8f6235156fb82089a5225dff4b58c0080d0f2236
aleksejgorobcov <lexa2987@mail.ru>
2024-01-17 00:42:43 +0300

    update config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,17 +7,9 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.eslint_d,
-				null_ls.builtins.diagnostics.erb_lint,
+				null_ls.builtins.formatting.eslint_d,
 				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.diagnostics.rubocop,
-				null_ls.builtins.formatting.rubocop,
 			},
-			--on_attach = function(client)
-			--	if client.resolved_capabilities.document_formatting then
-			--		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-			--	end
-			--end,
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,24 +2,23 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set modifiable")  -- Enable modifiability
+vim.cmd("set modifiable") -- Enable modifiability
 vim.g.mapleader = " "
 -- Enable clipboard support
-vim.cmd[[set clipboard=unnamedplus]]
+vim.cmd([[set clipboard=unnamedplus]])
 
 -- Ensure that '+clipboard' is available (requires Neovim to be compiled with clipboard support)
-if vim.fn.has('clipboard') == 1 then
-  -- Use system clipboard for all operations
-  vim.cmd[[set clipboard=unnamedplus]]
+if vim.fn.has("clipboard") == 1 then
+	-- Use system clipboard for all operations
+	vim.cmd([[set clipboard=unnamedplus]])
 end
 
-
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
+vim.keymap.set("n", "<leader>tn", ":tabnext<CR>")
 vim.wo.number = true
-
