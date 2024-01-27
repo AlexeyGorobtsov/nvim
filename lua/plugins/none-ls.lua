e967d2d8848ebb6c6a0f4c132ee5093f2b005758
aleksejgorobcov <lexa2987@mail.ru>
2024-01-27 22:58:12 +0300

    update config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,7 +6,10 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				--null_ls.builtins.formatting.prettier,
+			--null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettier.with({
+					filetypes = { "css" }, -- Specify filetypes for which Prettier should be used
+				}),
 				null_ls.builtins.formatting.eslint_d,
 				null_ls.builtins.diagnostics.eslint_d,
 			},
