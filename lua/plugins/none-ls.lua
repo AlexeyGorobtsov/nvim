6801e035d2b422b218709202c558c1df88ed52a8
aleksejgorobcov <lexa2987@mail.ru>
2024-04-12 22:42:39 +0300

    update config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -9,9 +9,7 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier.with({
-          filetypes = { "css" }, -- Specify filetypes for which Prettier should be used
-        }),
+        null_ls.builtins.formatting.prettier,
         require("none-ls.diagnostics.eslint_d"),
         require("none-ls.code_actions.eslint_d"),
         require("none-ls.formatting.eslint_d"),
@@ -19,5 +17,14 @@ return {
     })
 
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+    -- Keybind for ESLint's sort-imports fix
+    vim.keymap.set("n", "<leader>gs", function()
+      local eslint_d = null_ls.builtins.code_actions.eslint_d.with({
+        command = "eslint_d --fix-type=problem --stdin-filename ${INPUT}",
+        args = { "--fix-type", "problem" },
+      })
+      null_ls.run(eslint_d, { method = "textDocument/codeAction" })
+    end, { desc = "Sort imports with ESLint" })
   end,
 }
