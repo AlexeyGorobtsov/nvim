a61824b322b6731740a73b7df78d9f99f20b5a3a
18282607 <AViGorobtsov@sberbank.ru>
2024-05-07 21:39:02 +0300

    update config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,30 +1,25 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        require("none-ls.diagnostics.eslint_d"),
-        require("none-ls.code_actions.eslint_d"),
-        require("none-ls.formatting.eslint_d"),
-      },
-    })
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				require("none-ls.diagnostics.eslint_d"),
+				require("none-ls.code_actions.eslint_d"),
+				require("none-ls.formatting.eslint_d"),
+			},
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-
-    -- Keybind for ESLint's sort-imports fix
-    vim.keymap.set("n", "<leader>gs", function()
-      local eslint_d = null_ls.builtins.code_actions.eslint_d.with({
-        command = "eslint_d --fix-type=problem --stdin-filename ${INPUT}",
-        args = { "--fix-type", "problem" },
-      })
-      null_ls.run(eslint_d, { method = "textDocument/codeAction" })
-    end, { desc = "Sort imports with ESLint" })
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		-- Keybind for ESLint's "eslint:fix" command
+		 vim.keymap.set("n", "<leader>gs", function()
+      vim.cmd("!eslint --fix %")
+    end, { noremap = true, silent = true })
+	end,
 }
