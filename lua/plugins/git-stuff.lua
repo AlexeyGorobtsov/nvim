4712ee530b20653a81001352e74d2c78288cf9fe
aleksejgorobcov <lexa2987@mail.ru>
2024-05-21 22:22:15 +0300

    update commit

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -23,3 +23,31 @@ return {
 -- git rm -r src = rm -r src + git add src
 -- git rm -r --cashed src // untracked dir src (отсутствует в index)
 -- git mv index.html hello.html // переименовал файл index.html и добавил в рабочую директорию index
+-- git checkout -f master // флаг -f выбрасывает все изменения (удаляем незакомиченные изменения)
+-- git checkout -f HEAD // отменить все изменения сделанные в ветке-- (удаляем незакомиченные изменения)
+-- git stash // удаляет незакомиченные изменения и архивирует в git
+-- git stash pop - вернуть незакомиченные изменения
+-- git checkout -b fix // создаем и переключаемся на ветку
+-- если ошибочно сделали коммит в мастере
+-- 1) git branch fix // создаем новую ветку
+-- 2) git checkout fix // переходим на ветку fix
+-- 3) git branch -f master 54a4 переносим ветку мастер на коммит 54a4
+-- git branch -f master fix // если нужно вернуть назад master на коммит fix
+-- git checkout -B master 54a4 // если такая ветка есть он передвинет, а потом переключится 
+-- detached HEAD // Отделенный HEAD
+-- git checkout master 
+-- detached head - отделенный head
+-- git checkout master index.html - команда git checkout достает только указанный файл
+-- git reset index.html сбрасывает index для данного конкретного файла
+-- git log --oneline - выводит лог комитов
+-- git show index.html @~:index.html смотрим изменения в файле на предыдущем коммите
+-- git merge fix сливаем изменения из ветки fix в ветку мастер
+-- cat .git/ORIG_HEAD - команда merge перед переносом ветки записывает старый идентификатор в ORIG_HEAD
+-- git branch -f master ORIG_HEAD - возвращение мастера обратно до merge
+-- git branch -d fix - удаление ветки fix
+-- git reset --hard 2fad - передвигает текущую ветку на указанный коммит и обновляет рабочую 
+-- директорию вместе с index, так чтобы они соответствовали состоянию проекту
+-- git reset --hard - удаление не закомиченых изменений
+-- git reset --soft @~ возвращает мастер на предыдущий коммит 
+-- git commit -C ORIG_HEAD - C описание копируется из предыдущего коммита, с - откроется окно редактора
+--
