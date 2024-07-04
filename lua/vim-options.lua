eab4935d09bd369f5badac14bd5893a5b7ac1935
18282607 <AViGorobtsov@sberbank.ru>
2024-07-04 21:07:22 +0300

    update config

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,13 +4,14 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set modifiable") -- Enable modifiability
 vim.g.mapleader = " "
+
 -- Enable clipboard support
 vim.cmd([[set clipboard=unnamedplus]])
 
 -- Ensure that '+clipboard' is available (requires Neovim to be compiled with clipboard support)
 if vim.fn.has("clipboard") == 1 then
-	-- Use system clipboard for all operations
-	vim.cmd([[set clipboard=unnamedplus]])
+    -- Use system clipboard for all operations
+    vim.cmd([[set clipboard=unnamedplus]])
 end
 
 -- Navigate vim panes better
@@ -22,9 +23,9 @@ vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 vim.keymap.set("n", "<leader>tn", ":tabnext<CR>")
 vim.wo.number = true
+
 -- Включает проверку правописания
 vim.opt.spell = true
 
 -- Установка языка проверки правописания на английский и любой другой по вашему выбору
-vim.opt.spelllang = {"en_us", "ru_ru"}
-
+vim.opt.spelllang = { "en_us", "ru_ru" }
