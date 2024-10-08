f9ba8cb8d1cf08f965962c3e7804a143b5c8dfa6
18282607 <AViGorobtsov@sberbank.ru>
2024-10-08 16:29:26 +0300

    update config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,38 +9,96 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		lazy = false,
-		opts = {
-			auto_install = true,
-		},
+		opts = { auto_install = true },
 	},
 	{
 		"neovim/nvim-lspconfig",
 		lazy = false,
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
 			local lspconfig = require("lspconfig")
 
-			lspconfig.tsserver.setup({
+			-- Existing LSP configurations
+			lspconfig.ts_ls.setup({
 				capabilities = capabilities,
+				init_options = {
+					preferences = {
+						importModuleSpecifierPreference = "relative",
+					},
+				},
 			})
-			lspconfig.html.setup({
+			lspconfig.html.setup({ capabilities = capabilities })
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.jsonls.setup({ capabilities = capabilities })
+			lspconfig.cssls.setup({ capabilities = capabilities })
+
+			-- Updated Groovy LSP configuration
+			local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
+			local groovyls_jar = mason_path
+				.. "packages/groovy-language-server/build/libs/groovy-language-server-all.jar"
+
+			lspconfig.groovyls.setup({
 				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
+				cmd = { "java", "-jar", groovyls_jar },
+				filetypes = { "groovy" },
+				root_dir = lspconfig.util.root_pattern("*.gradle", "*.groovy", ".git"),
+				settings = {
+					groovy = {
+						classpath = {
+							-- Add any additional classpath entries here
+						},
+						console = {
+							-- Configure console settings if needed
+						},
+						trace = {
+							server = "verbose",
+						},
+					},
+				},
+				on_attach = function(client, bufnr)
+					print("groovyls attached to buffer " .. bufnr)
+
+					-- Set up buffer-local keybindings here
+					local opts = { noremap = true, silent = true, buffer = bufnr }
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				end,
+				on_init = function(client)
+					print("groovyls initialized")
+				end,
 			})
 
+			-- Diagnostic configuration
+			-- vim.diagnostic.config({
+			-- 	virtual_text = true,
+			-- 	signs = true,
+			-- 	underline = true,
+			-- 	update_in_insert = false,
+			-- 	severity_sort = false,
+			-- })
+
+			-- Set up global keybindings
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+
+			-- LSP logging configuration
+			-- vim.lsp.set_log_level("debug")
+			-- require("vim.lsp.log").set_format_func(vim.inspect)
+			--
+			-- Custom command to restart Groovy LSP
+			vim.api.nvim_create_user_command("RestartGroovyLSP", function()
+				vim.cmd("LspStop groovyls")
+				vim.cmd("sleep 500m") -- Wait for 500ms
+				vim.cmd("LspStart groovyls")
+			end, {})
 		end,
 	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,25 +1,97 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local conform = require("conform")
+			conform.setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescriptreact = { "prettier" },
+					css = { "prettier" },
+					html = { "prettier" },
+					json = { "prettier" },
+					yaml = { "prettier" },
+					markdown = { "prettier" },
+					groovy = { "npm_groovy_lint" },
+					-- Add other formatters as needed
+				},
+				formatters = {
+					npm_groovy_lint = {
+						command = "npm-groovy-lint",
+						args = { "--format", "$FILENAME" },
+						cwd = require("conform.util").root_file({ ".groovylintrc.json", ".groovylintrc.js" }),
+					},
+				},
+				format_on_save = {
+					timeout_ms = 5000, -- Increased timeout
+					lsp_fallback = true,
+				},
+			})
+
+			vim.keymap.set({ "n", "v" }, "<leader>gf", function()
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 5000, -- Increased timeout
+				})
+			end, { desc = "Format file or range (in visual mode)" })
+		end,
 	},
-	config = function()
-		local null_ls = require("null-ls")
+	{
+		"mfussenegger/nvim-lint",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local lint = require("lint")
+			lint.linters_by_ft = {
+				javascript = { "eslint" },
+				typescript = { "eslint" },
+				javascriptreact = { "eslint" },
+				typescriptreact = { "eslint" },
+				groovy = { "npm_groovy_lint" },
+				-- Add other linters as needed
+			}
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				require("none-ls.diagnostics.eslint_d"),
-				require("none-ls.code_actions.eslint_d"),
-				require("none-ls.formatting.eslint_d"),
-			},
-		})
+			lint.linters.npm_groovy_lint = {
+				cmd = "npm-groovy-lint",
+				args = { "--linter", "$FILENAME" },
+				ignore_exitcode = true,
+				parser = function(output, bufnr)
+					local diagnostics = {}
+					for line in output:gmatch("[^\r\n]+") do
+						local severity, message, lnum, col = line:match("([^|]+)|([^|]+)|(%d+)|(%d+)")
+						if severity and message and lnum and col then
+							table.insert(diagnostics, {
+								source = "npm-groovy-lint",
+								lnum = tonumber(lnum) - 1,
+								col = tonumber(col) - 1,
+								end_lnum = tonumber(lnum) - 1,
+								end_col = tonumber(col),
+								severity = vim.diagnostic.severity[severity:upper()] or vim.diagnostic.severity.WARN,
+								message = message:gsub("^%s*(.-)%s*$", "%1"),
+								bufnr = bufnr,
+							})
+						end
+					end
+					return diagnostics
+				end,
+			}
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-		-- Keybind for ESLint's "eslint:fix" command
-		 vim.keymap.set("n", "<leader>gs", function()
-      vim.cmd("!eslint --fix %")
-    end, { noremap = true, silent = true })
-	end,
+			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+				group = lint_augroup,
+				callback = function()
+					lint.try_lint()
+				end,
+			})
+
+			vim.keymap.set("n", "<leader>gl", function()
+				lint.try_lint()
+			end, { desc = "Trigger linting for current file" })
+		end,
+	},
 }
