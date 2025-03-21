18a5ba101c49c67d5a6a958e8f8ae849c5c9e82f
aleksejgorobcov <lexa2987@mail.ru>
2025-03-21 10:06:48 +0300

    update

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,32 +1,43 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.cmd("set modifiable") -- Enable modifiability
+-- Basic editor settings
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.tabstop = 2 -- Number of spaces a tab counts for
+vim.opt.softtabstop = 2 -- Number of spaces for a tab while editing
+vim.opt.shiftwidth = 2 -- Number of spaces for each indentation level
+vim.opt.modifiable = true -- Allow buffer to be modified
+vim.opt.number = true -- Show line numbers
+vim.opt.relativenumber = true -- Show relative line numbers for easier navigation
+vim.opt.scrolloff = 8 -- Keep 8 lines visible above/below cursor when scrolling
+vim.opt.smartindent = true -- Smart auto-indenting
+vim.opt.syntax = "on" -- Enable syntax highlighting
+vim.o.termguicolors = true -- Enable true color support
+
+-- Set space as the leader key
 vim.g.mapleader = " "
 
--- Enable clipboard support
-vim.cmd([[set clipboard=unnamedplus]])
+-- Clipboard settings
+vim.opt.clipboard = "unnamedplus" -- Use system clipboard
 
--- Ensure that '+clipboard' is available (requires Neovim to be compiled with clipboard support)
-if vim.fn.has("clipboard") == 1 then
-	-- Use system clipboard for all operations
-	vim.cmd([[set clipboard=unnamedplus]])
-end
+-- Color scheme
+-- vim.cmd("colorscheme desert")      -- Set color scheme to desert
 
--- Navigate vim panes better
-vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+-- Spell checking
+vim.opt.spell = true -- Enable spell checking
+vim.opt.spelllang = { "en_us", "ru_ru" } -- Set spell check languages to English and Russian
 
-vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
-vim.keymap.set("n", "<leader>tn", ":tabnext<CR>")
-vim.wo.number = true
+-- Keymaps for window navigation
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>", { desc = "Move to window above" })
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>", { desc = "Move to window below" })
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>", { desc = "Move to window left" })
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>", { desc = "Move to window right" })
 
--- Включает проверку правописания
-vim.opt.spell = true
+-- Custom keymaps
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "Clear search highlighting" })
+vim.keymap.set("n", "<leader>tn", ":tabnext<CR>", { desc = "Go to next tab" })
+vim.keymap.set("n", "<leader>pv", ":Vex<CR>", { noremap = true, desc = "Open vertical explorer" })
 
--- Установка языка проверки правописания на английский и любой другой по вашему выбору
-vim.opt.spelllang = { "en_us", "ru_ru" }
-vim.o.termguicolors = true
+-- Paste from yank register (register 0) instead of default register
+-- This preserves what you've copied when you delete something and then paste
+vim.keymap.set("n", "p", '"0p', { noremap = true, desc = "Paste from yank register (normal mode)" })
+vim.keymap.set("n", "P", '"0P', { noremap = true, desc = "Paste before cursor from yank register (normal mode)" })
+vim.keymap.set("v", "p", '"0p', { noremap = true, desc = "Paste from yank register (visual mode)" })
+vim.keymap.set("v", "P", '"0P', { noremap = true, desc = "Paste before cursor from yank register (visual mode)" })
